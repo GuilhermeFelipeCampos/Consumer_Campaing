29f55c55ac58ef51bf601671ff5b817fead2c032
Guilherme Campos <guilherme.campos@ifood.com.br>
2024-06-28 13:20:04 -0300

    fix dockerfile and docker compose

--- database/squemas/squema_db.sql
+++ database/squemas/squema_db.sql
@@ -5,7 +5,8 @@ GRANT ALL PRIVILEGES ON DATABASE "consumer-campaing-pg" TO "postgres";
 GRANT USAGE ON SCHEMA consumer_campaing_pg TO "postgres";
 ALTER USER "postgres" SET search_path = 'consumer_campaing_pg';
 
-SET SCHEMA 'consumer-campaing-pg';
+
+SET SCHEMA 'consumer_campaing_pg';
 ALTER DEFAULT PRIVILEGES
     IN SCHEMA consumer_campaing_pg
 GRANT SELECT, UPDATE, INSERT, DELETE ON TABLES
---
+++ repository/database_test.go
@@ -0,0 +1,29 @@
+package repository
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+func TestGetConnection(t *testing.T) {
+	type args struct {
+		ctx context.Context
+	}
+	tests := []struct {
+		name string
+		args args
+		want *pgxpool.Pool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetConnection(tt.args.ctx); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetConnection() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
